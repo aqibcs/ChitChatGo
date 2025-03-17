679b1e86be1a94dfdcbfba7a720be4f8aae558b2
aqibcs <csaqibshah@gmail.com>
2025-03-17 08:53:37 +0100

    Enhanced admin panel settings for chat-related models

--- chat/admin.py
+++ chat/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, FriendRequest, Friendship, ChatGroup, Message, Notification
 
 admin.site.register(Profile)
+admin.site.register(FriendRequest)
+admin.site.register(Friendship)
+admin.site.register(ChatGroup)
+admin.site.register(Message)
+admin.site.register(Notification)
